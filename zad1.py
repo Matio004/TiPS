1dd1b98d5541c165608ecf5ef2314f096c412dbc
Matio004 <mateuszkupis@interia.eu>
2025-04-05 18:57:41 +0200

    coding

--- zad1.py
+++ zad1.py
@@ -12,3 +12,13 @@ H = numpy.array(
         [1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1]
     ], dtype=numpy.int64
 )
+print(H[:, :8])
+def coding(text):
+    result = []
+    for char in text:
+        data_bit = [int(bit) for bit in format(ord(char), '08b')]
+        check_bit = (numpy.dot(H[:, :8], data_bit) % 2).tolist()
+        result.append(data_bit + check_bit)
+    return result
+
+
