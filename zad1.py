089d241a3e79887fee4ef6d42d40c110bfcdac83
Maja Mejza <251588@edu.p.lodz.pl>
2025-04-06 12:02:07 +0200

    check encoded, decode

--- zad1.py
+++ zad1.py
@@ -13,12 +13,32 @@ H = numpy.array(
     ], dtype=numpy.int64
 )
 print(H[:, :8])
-def coding(text):
+def encoding(text):
     result = []
     for char in text:
         data_bit = [int(bit) for bit in format(ord(char), '08b')]
         check_bit = (numpy.dot(H[:, :8], data_bit) % 2).tolist()
         result.append(data_bit + check_bit)
     return result
+
+def check_encoded(encoded_text):
+    result = []
+    for char in encoded_text:
+        syndrome = numpy.dot(H, char) % 2
+        if any(syndrome):
+            for i in range(16):
+                char[i] = 1 - char[i]
+                break
+            result.append(char[:8])
+        return result
+
+def decoding(decoded_text):
+    result = []
+    for byte in decoded_text:
+        char = chr(int(''.join(map(str,byte)), 2))
+        result.append(char)
+    return ''.join(result)
+
+
 
 
